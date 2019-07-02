4707c4a6411a4de19c468e38cee585134c16e5bc
AC03 <oderocharlesouma@gmail.com>
2019-07-02 17:03:55 +0300

    The second update

--- app/src/main/java/com/e/calculator_app/MainActivity.java
+++ app/src/main/java/com/e/calculator_app/MainActivity.java
@@ -2,13 +2,50 @@ package com.e.calculator_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView myScreen;
+    private Button   one;
+    private Button   two;
+    private Button   three;
+    private Button   four;
+    private Button   five;
+    private Button   six;
+    private Button   seven;
+    private Button   eight;
+    private Button   nine;
+    private Button   zero;
+    private Button   point;
+    private Button   equal;
+    private Button   multiplication;
+    private Button   division;
+    private Button   add;
+    private Button   subtraction;
+    private Button   cancel;
+    private Button   delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
 }
